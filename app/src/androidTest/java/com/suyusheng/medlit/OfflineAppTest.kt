0f5996525afe1fcee3c78789e5ba21f84a264bde
sueivan <36700274+sueivan@users.noreply.github.com>
2026-09-25 01:42:34 +0800

    fix: compile WebView visibility test

--- app/src/androidTest/java/com/suyusheng/medlit/OfflineAppTest.kt
+++ app/src/androidTest/java/com/suyusheng/medlit/OfflineAppTest.kt
@@ -2,14 +2,12 @@ package com.suyusheng.medlit
 
 import androidx.test.espresso.web.assertion.WebViewAssertions.webMatches
 import androidx.test.espresso.web.sugar.Web.onWebView
-import androidx.test.espresso.web.webdriver.DriverAtoms.getAttribute
 import androidx.test.espresso.web.webdriver.DriverAtoms.findElement
 import androidx.test.espresso.web.webdriver.DriverAtoms.getText
 import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matchers.containsString
-import org.hamcrest.Matchers.equalTo
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -29,7 +27,7 @@ class OfflineAppTest {
     @Test
     fun pwaInstallCardIsHidden() {
         onWebView()
-            .withElement(findElement(Locator.ID, "install-app-shell"))
-            .check(webMatches(getAttribute("hidden"), equalTo("true")))
+            .withElement(findElement(Locator.CSS_SELECTOR, "#install-app-shell[hidden]"))
+            .check(webMatches(getText(), containsString("安装")))
     }
 }
