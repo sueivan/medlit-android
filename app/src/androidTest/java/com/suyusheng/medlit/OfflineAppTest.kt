c56eecd3c721c5a57d5225d3803daa0014252355
sueivan <36700274+sueivan@users.noreply.github.com>
2026-09-25 01:26:12 +0800

    test: define offline WebView behavior

---
+++ app/src/androidTest/java/com/suyusheng/medlit/OfflineAppTest.kt
@@ -0,0 +1,35 @@
+package com.suyusheng.medlit
+
+import androidx.test.espresso.web.assertion.WebViewAssertions.webMatches
+import androidx.test.espresso.web.model.Atoms.getStyleProperty
+import androidx.test.espresso.web.model.Atoms.getText
+import androidx.test.espresso.web.sugar.Web.onWebView
+import androidx.test.espresso.web.webdriver.DriverAtoms.findElement
+import androidx.test.espresso.web.webdriver.Locator
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.containsString
+import org.hamcrest.Matchers.equalTo
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class OfflineAppTest {
+    @get:Rule
+    val scenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun coldStartLoadsCourseHome() {
+        onWebView()
+            .withElement(findElement(Locator.ID, "home-title"))
+            .check(webMatches(getText(), containsString("从一个问题")))
+    }
+
+    @Test
+    fun pwaInstallCardIsHidden() {
+        onWebView()
+            .withElement(findElement(Locator.ID, "install-app-shell"))
+            .check(webMatches(getStyleProperty("display"), equalTo("none")))
+    }
+}
